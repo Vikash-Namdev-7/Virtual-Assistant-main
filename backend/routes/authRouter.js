a5bf06969b075b4394c4b6d82866a7aa562df3d7
Vikash Namdev <vikashnamdev1111@gmail.com>
2026-06-11 20:14:13 +0530

    auth fix

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Login, logOut, signUp } from "../controllers/Auth.js"
+import { Login, logOut, signUp } from "../controllers/auth.js"
 
 const authRouter = express.Router();
 
